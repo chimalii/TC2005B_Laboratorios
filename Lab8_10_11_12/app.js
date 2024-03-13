cea9d9359da7c464b55d572b23087901e4c197ff
Chimali Nava <chimalinava@gmail.com>
2024-03-13 15:25:33 -0600

    creación de partials para generer html del template  ejs

--- Lab8_10_11_12/app.js
+++ Lab8_10_11_12/app.js
@@ -4,6 +4,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -18,7 +21,6 @@ const rutasClases= require('./routes/clases.routes');
 
 app.use('/', rutasClases);
 
-const path = require('path');
 app.use((request, response, next) => {
   response.status(404);
   response.sendFile(path.join(__dirname, 'views', '404.html')); 
