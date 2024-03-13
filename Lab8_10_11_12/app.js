f6a0149fe86bf59f7bdae4960d797016e8b549af
Chimali Nava <chimalinava@gmail.com>
2024-03-13 02:01:23 -0600

    instalar y configurar ejs

--- Lab8_10_11_12/app.js
+++ Lab8_10_11_12/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
 /*/Middleware
